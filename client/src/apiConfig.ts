015c6dcfee74e236a96db9027f1af651998504af
Dominiq Martinez <dominiqmartinez13@gmail.com>
2024-03-21 11:59:59 -0700

    pushing main

--- client/src/apiConfig.ts
+++ client/src/apiConfig.ts
@@ -1,5 +1,5 @@
 export const urls = {
   local: 'http://localhost:4000/rest',
-  development: 'https://vccrm-473bc49906bc.herokuapp.com/rest',
-  production: 'https://vccrm-473bc49906bc.herokuapp.com/rest'
+  development: 'https://voltaiccrm-7dd827fb5012.herokuapp.com/rest',
+  production: 'https://voltaiccrm-7dd827fb5012.herokuapp.com/rest'
 };
