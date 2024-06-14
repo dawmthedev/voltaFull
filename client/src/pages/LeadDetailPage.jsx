f7ef65d3e7906699244cdc325be917449780eaf3
Dominiq <marti958@cougars.csusm>
2024-06-14 10:16:51 -0700

    fix

--- client/src/pages/LeadDetailPage.jsx
+++ client/src/pages/LeadDetailPage.jsx
@@ -395,7 +395,6 @@ const LeadDetailPage = () => {
               year: '2-digit',
               hour: '2-digit',
               minute: '2-digit',
-              second: '2-digit',
               hour12: true,
               hourCycle: 'h12'
             })
