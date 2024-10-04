7ba84c861be8e55178e0509068d5edee1a41e918
Dominiq <marti958@cougars.csusm>
2024-10-04 01:26:55 -0700

    fix

--- client/src/components/BatteryStepper.js
+++ client/src/components/BatteryStepper.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Stepper, Step, StepLabel, Typography, Modal, Box } from '@mui/material';
 
+
+//Change to front end
 const stepLabels = [
     { label: 'Battery Permit', key: 'batteryPermitDate' },
     { label: 'Battery Approval', key: 'batteryApprovalDate' },
