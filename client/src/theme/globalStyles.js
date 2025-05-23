49759a41b432ba0ab5620eebfeedfd87c0c8f8ce
dawmthedev <dominiqmartinez13@gmail.com>
2025-05-23 01:14:08 -0700

    Fix globalStyles object structure

--- client/src/theme/globalStyles.js
+++ client/src/theme/globalStyles.js
@@ -15,8 +15,17 @@ export default function GlobalStyles() {
           width: '100%',
           height: '100%',
           WebkitOverflowScrolling: 'touch',
+        },
         body: {
+          margin: 0,
+          padding: 0,
+          width: '100%',
+          height: '100%',
+        },
         '#root': {
+          width: '100%',
+          height: '100%',
+        },
         input: {
           '&[type=number]': {
             MozAppearance: 'textfield',
@@ -25,13 +34,22 @@ export default function GlobalStyles() {
               WebkitAppearance: 'none',
             },
             '&::-webkit-inner-spin-button': {
+              margin: 0,
+              WebkitAppearance: 'none',
+            },
           },
+        },
         img: {
           display: 'block',
           maxWidth: '100%',
+        },
         ul: {
+          margin: 0,
+          padding: 0,
+        },
       }}
     />
   );
+
   return inputGlobalStyles;
 }
