d5474d355bd8c4d5b74c374df1d81c22cd4bdc6f
dawmthedev <dominiqmartinez13@gmail.com>
2025-05-22 22:13:46 -0700

    Fix AppNewsUpdate component

--- client/src/sections/@dashboard/app/AppNewsUpdate.js
+++ client/src/sections/@dashboard/app/AppNewsUpdate.js
@@ -20,34 +20,35 @@ export default function AppNewsUpdate({ title, subheader, list, ...other }) {
       <CardHeader title={title} subheader={subheader} />
       <Scrollbar>
         <Stack spacing={3} sx={{ p: 3, pr: 0 }}>
-          {list.map((news) => (<>
-  
-                <NewsItem key={news.id} news={news} />
-          </>
-      
+          {list.map((news) => (
+            <NewsItem key={news.id} news={news} />
           ))}
         </Stack>
       </Scrollbar>
       <Divider />
       <Box sx={{ p: 2, textAlign: 'right' }}>
-        <Button size="small" color="inherit" endIcon={<Iconify icon={'eva:arrow-ios-forward-fill'} />}>
+        <Button
+          size="small"
+          color="inherit"
+          endIcon={<Iconify icon={'eva:arrow-ios-forward-fill'} />}
+        >
           View all
         </Button>
       </Box>
     </Card>
   );
 }
-NewsItem.propTypes = {
-  news: PropTypes.shape({
-    description: PropTypes.string,
-    image: PropTypes.string,
-    postedAt: PropTypes.instanceOf(Date),
-    title: PropTypes.string,
-  }),
+
 function NewsItem({ news }) {
   const { image, title, description, postedAt } = news;
+  return (
     <Stack direction="row" alignItems="center" spacing={2}>
-      <Box component="img" alt={title} src={image} sx={{ width: 48, height: 48, borderRadius: 1.5, flexShrink: 0 }} />
+      <Box
+        component="img"
+        alt={title}
+        src={image}
+        sx={{ width: 48, height: 48, borderRadius: 1.5, flexShrink: 0 }}
+      />
       <Box sx={{ minWidth: 240, flexGrow: 1 }}>
         <Link color="inherit" variant="subtitle2" underline="hover" noWrap>
           {title}
@@ -55,10 +56,25 @@ function NewsItem({ news }) {
         <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
           {description}
         </Typography>
-      <Button href=""  className={styles.callButton}>
-      <Iconify icon="eva:phone-fill" color="#18712" width={22} height={22}  />
-              </Button>
-      <Typography variant="caption" sx={{ pr: 3, flexShrink: 0, color: 'text.secondary' }}>
+      </Box>
+      <Button href="" className={styles.callButton}>
+        <Iconify icon="eva:phone-fill" color="#18712" width={22} height={22} />
+      </Button>
+      <Typography
+        variant="caption"
+        sx={{ pr: 3, flexShrink: 0, color: 'text.secondary' }}
+      >
         {fToNow(postedAt)}
       </Typography>
     </Stack>
+  );
+}
+
+NewsItem.propTypes = {
+  news: PropTypes.shape({
+    description: PropTypes.string,
+    image: PropTypes.string,
+    postedAt: PropTypes.instanceOf(Date),
+    title: PropTypes.string,
+  }),
+};
