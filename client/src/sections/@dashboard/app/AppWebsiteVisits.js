6b30cb229307ccf9a99eb3a0ad3e43e1fe172ab8
dawmthedev <dominiqmartinez13@gmail.com>
2025-05-23 15:35:55 -0700

    chore: import Card components

--- client/src/sections/@dashboard/app/AppWebsiteVisits.js
+++ client/src/sections/@dashboard/app/AppWebsiteVisits.js
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Card, CardHeader } from '@mui/material';
 import PropTypes from 'prop-types';
 import ReactApexChart from 'react-apexcharts';
 // @mui
