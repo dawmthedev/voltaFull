94ae5d8806712d094b050e7229c3c916420e3f91
dawmthedev <dominiqmartinez13@gmail.com>
2025-05-23 18:34:16 -0700

    Remove dashboard chart components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { LicenseInfo } from '@mui/x-license-pro';
 import Router from './routes';
 import ThemeProvider from './theme';
 import ScrollToTop from './components/scroll-to-top';
-import { StyledChart } from './components/chart';
 import { Alerts } from './components/alert/Alert';
 
 LicenseInfo.setLicenseKey('9e17734200a964cd420488accda5490fTz01ODkyOSxFPTE3MDY4NzA0MzEyMTAsUz1wcm8sTE09c3Vic2NyaXB0aW9uLEtWPTI=');
@@ -13,7 +12,6 @@ export default function App() {
     <ThemeProvider>
       <Alerts />
       <ScrollToTop />
-      <StyledChart />
       <Router />
     </ThemeProvider>
   );
