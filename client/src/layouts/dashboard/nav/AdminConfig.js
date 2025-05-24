7b1edc7fefb497aba22caeea6e9f69a1af43254a
dawmthedev <dominiqmartinez13@gmail.com>
2025-05-23 17:06:34 -0700

    Simplify dashboard nav

--- client/src/layouts/dashboard/nav/AdminConfig.js
+++ client/src/layouts/dashboard/nav/AdminConfig.js
@@ -8,67 +8,9 @@ const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ 
 const AdminConfig = [
   {
     title: 'Dashboard',
-    path: '/dashboard/app',
-    icon: icon('ic_homet')
-  },
-
-  {
-    title: 'Projects',
-    path: '/dashboard/deals',
+    path: '/dashboard',
     icon: icon('ic_cart')
-  },
-  
-  {
-    title: 'Pay',
-    path: '/dashboard/pay',
-    icon: icon('ic_user')
-  },
-  // {
-  //   title: 'Users',
-  //   path: '/dashboard/user',
-  //   icon: icon('ic_user')
-  // },
-  {
-    title: 'Resources',
-    path: '/dashboard/blog',
-    icon: icon('ic_blog')
-  },
-
-  {
-    title: 'Rookies',
-    path: '/dashboard/Q',
-    icon: icon('ic_cart')
-  },
-  // {
-  //   title: 'Listings',
-  //   path: '/dashboard/products',
-  //   icon: icon('ic_cart')
-  // },
-  // {
-  //   title: 'blog',
-  //   path: '/dashboard/blog',
-  //   icon: icon('ic_blog')
-  // },
-  // {
-  //   title: 'login',
-  //   path: '/login',
-  //   icon: icon('ic_lock')
-  // },
-  // {
-  //   title: 'Leads',
-  //   path: '/dashboard/leads',
-  //   icon: icon('ic_blog')
-  // },
-  // {
-  //   title: 'Dynamic-Leads',
-  //   path: '/dashboard/dynamic-leads',
-  //   icon: icon('ic_analytics')
-  // },
-  // {
-  //   title: 'Not found',
-  //   path: '/404',
-  //   icon: icon('ic_disabled')
-  // }
+  }
 ];
 
 export default AdminConfig;
