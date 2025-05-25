1c6c6849f6ba13340a84483a81c8f8c426f084b5
dawmthedev <dominiqmartinez13@gmail.com>
2025-05-25 14:55:53 -0700

    fix(client-2): use server base url for auth

--- client-2/src/api/auth.ts
+++ client-2/src/api/auth.ts
@@ -4,8 +4,10 @@ export interface Credentials {
   password: string;
 }
 
+import { baseURL } from '../apiConfig';
+
 export async function login(credentials: Credentials) {
-  const response = await fetch('/rest/auth/login', {
+  const response = await fetch(`${baseURL}/auth/login`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
---
+++ client-2/src/apiConfig.ts
@@ -0,0 +1,8 @@
+export const urls = {
+  local: 'http://localhost:4000/rest',
+  development: 'http://localhost:4000/rest',
+  production: 'https://voltaiccrm-7dd827fb5012.herokuapp.com/rest'
+};
+
+const env = process.env.REACT_APP_STAGE || 'development';
+export const baseURL = urls[env] || urls.development;
