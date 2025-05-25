4dd4a4cd72d28d47bd925f6b10e525579fb2fb21
dawmthedev <dominiqmartinez13@gmail.com>
2025-05-25 01:16:04 -0700

    Remove all tests and Jest configs

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  projects: [
-    '<rootDir>/client/jest.config.js',
-    '<rootDir>/server/jest.config.js'
-  ],
-  testResultsProcessor: './scripts/process-jest-results.js'
-
-};
--- tests/client/root/RootComponent.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { RootComponent } from '../../../client/src/index';
-
-describe('RootComponent', () => {
-  it('renders hello world', () => {
-    render(<RootComponent />);
-    expect(screen.getByText(/hello world/i)).toBeTruthy();
-  });
-});
