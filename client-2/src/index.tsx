0090a3e3894f760721d8bb5fa102da4173ff2c6f
dawmthedev <dominiqmartinez13@gmail.com>
2025-05-25 14:48:28 -0700

    Remove redundant AuthProvider wrapper

--- client-2/src/index.tsx
+++ client-2/src/index.tsx
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Provider } from './components/ui/provider';
-import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 );
