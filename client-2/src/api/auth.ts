680b5cf442f13e85e1efa3b829e99694f5413d90
dawmthedev <dominiqmartinez13@gmail.com>
2025-05-25 14:29:16 -0700

    Add login API for client-2

---
+++ client-2/src/api/auth.ts
@@ -0,0 +1,21 @@
+export interface Credentials {
+  email: string;
+  password: string;
+}
+
+export async function login(credentials: Credentials) {
+  const response = await fetch('/rest/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to login');
+  }
+
+  return response.json();
+}
+
