28ef938594d58e70a9d5c8cc851a8028cdff62e8
dawmthedev <dominiqmartinez13@gmail.com>
2025-05-24 21:35:19 -0700

    fix(tsconfig): adjust typeRoots and use ts-jest

--- server/jest.config.js
+++ server/jest.config.js
@@ -52,6 +52,6 @@ module.exports = {
   ],
   // A map from regular expressions to paths to transformers
   transform: {
-    '\\.(ts)$': 'ts-jest'
+    '\\.(ts)$': ['ts-jest', { tsconfig: './tsconfig.json' }]
   }
 }
