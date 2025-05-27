fa4c11cfdb661adfba181379c996f39363ecad5b
dawmthedev <dominiqmartinez13@gmail.com>
2025-05-26 23:52:45 -0700

    [VOLTA] align role check for user service

--- server/src/services/UserService.ts
+++ server/src/services/UserService.ts
@@ -3,6 +3,7 @@ import { Unauthorized } from "@tsed/exceptions";
 import { MongooseModel } from "@tsed/mongoose";
 import { AdminModel } from "../models/AdminModel";
 import { JWTPayload } from "../../types";
+import { ADMIN } from "../util/constants";
 
 
 @Injectable()
@@ -10,7 +11,8 @@ export class UserService {
   constructor(@Inject(AdminModel) private userModel: MongooseModel<AdminModel>) {}
 
   async findAll(user?: JWTPayload) {
-    if (!user || user.role !== "Admin") {
+    const role = user?.role;
+    if (!role || (role !== ADMIN && role.toLowerCase() !== "admin")) {
       throw new Unauthorized("Access denied");
     }
     return this.userModel.find();
